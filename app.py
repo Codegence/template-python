2fb4fe5530520c056a53afb3a7a38dc6b95b0a08
Fernando Farias <yiowns@gmail.com>
2014-11-01 14:00:04 -0300

    Clean up.

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 #!flask/bin/python
+
+#    Copyright (C) 2014  Gabriel Flores
+#    This file is part of Codegence.
+#
+#    Codegence is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Codegence is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Codegence. If not, see <http://www.gnu.org/licenses/>.
+
 from flask import Flask, jsonify, request
 import random
 
@@ -10,23 +27,22 @@ def index():
 
 @app.route('/info')
 def info():
-	return jsonify({'version':'1.1.0'})
+    return jsonify({'version':'1.1.0'})
 
 @app.route('/sectors/<int:sectorId>/factions/<int:factionId>/recyclers/<int:id>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def eventOnRecycler(sectorId, factionId, id):	
-	print request.json
-
-	return jsonify({'command': random.choice(['newDrone', 'newTerminator']), 'value':0.0})
+    print request.json
+    return jsonify({'command': random.choice(['newDrone', 'newTerminator']), 'value':0.0})
 
 @app.route('/sectors/<int:sectorId>/factions/<int:factionId>/drones/<int:id>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def eventOnDrone(sectorId, factionId, id):
-	print request.json
-	return jsonify({'command':random.choice(['advance', 'rotate']), 'value':10.0})
+    print request.json
+    return jsonify({'command':random.choice(['advance', 'rotate']), 'value':10.0})
 
 @app.route('/sectors/<int:sectorId>/factions/<int:factionId>/terminators/<int:id>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def eventOnTerminator(sectorId, factionId, id):
-	print request.json
-	return jsonify({'command':random.choice(['advance', 'rotate', 'fire']), 'value':10.0})
+    print request.json
+    return jsonify({'command':random.choice(['advance', 'rotate', 'fire']), 'value':10.0})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
